ea5f4ac1f353e3cc5f3bbd78d0477589a4a8f6f5
spacegaucho <vodka3dg@gmail.com>
2023-02-25 17:10:06 -0300

    mod: upgraded setup script

--- setup.sh
+++ setup.sh
@@ -2,31 +2,26 @@
 # DESCRIPTION: Install/setup minimum customization for bash.
 
 # export the desired value before installing
-VIM_BASH_ONLY=${VIM_BASH_ONLY:-0}
-INSTALL_STARSHIP=${INSTALL_STARSHIP:-1}
+COMMAND_OUT=/tmp/desksetup.out
 
-install_workstuff() { 
-  # Apt stuff
+main() { 
+  echo "I: starting installation.."
+  set -x
+  # Install basics + awscli and terraform
   DEBIAN_FRONTEND=noninteractive
+  sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get clean
   sudo apt-get update -y
-
-  sudo apt-get install -y software-properties-common git curl wget gnupg \
-  openssl vim neovim bash-completion python3 python3-pip python3-virtualenv \
-  golang ca-certificates gnupg lsb-release tmux man
-
-  # Fix: check if key exists
+  sudo apt-get install -y software-properties-common git curl wget gnupg openssl vim neovim bash-completion python3 python3-pip python3-virtualenv golang ca-certificates gnupg lsb-release tmux man
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-  | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt-get update 
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg  | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   sudo apt-get install -y awscli terraform
+  
+  # Personal hack
   mkdir ${HOME}/.aws
   touch ${HOME}/.aws/credentials
-
+  
   # Install kubectl
   curl -Lso kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   sudo chmod 755 kubectl 
@@ -41,7 +36,6 @@ install_workstuff() {
   if ! curl -s https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash; then echo "W: helm install failed!"; fi
 
   # Install docker-ce
-  DEBIAN_FRONTEND=noninteractive
   if ! sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y; then echo "W: docker install fialed!"; fi
 
   # Change docker BIP
@@ -63,28 +57,24 @@ EOF
   
   # Install k3d
   curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sudo bash
-}
 
-install_starship() {
-	# Install starship
+  # Install fzf
+  if ! git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; then echo "W: fialed clone fzf, exists?"; fi
+  ~/.fzf/install --all
+
+  # Install and configure starship
 	mkdir ~/.config || true
 	curl -s https://gist.githubusercontent.com/spacegaucho/b3a0be246d2af3a7bde323a587408f12/raw > ~/.config/starship.toml
 	curl -s https://starship.rs/install.sh -o install_starship.sh
 	sh install_starship.sh -y
 	rm install_starship.sh
-}
 
-install_vim_bash_custom() {
   # Vim stuff
   if ! mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle; then echo "W: failed mkdir for patogen, exists?"; fi
 	curl -LSso ${HOME}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
   curl -LSso ${HOME}/.vimrc https://gist.github.com/spacegaucho/2fef5c3f386086c2ff937fc85c92f21d/raw
   if ! mkdir -p ${HOME}/.vim/bundle/lightline.vim ; then echo "W: fialed mkdir lightline.vim, exists?"; fi
 	if ! git clone https://github.com/itchyny/lightline.vim ${HOME}/.vim/bundle/lightline.vim; then echo "W: failed clone ligthline.vim, exists?"; fi
-
-  # Install fzf
-  if ! git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; then echo "W: fialed clone fzf, exists?"; fi
-  ~/.fzf/install --all
 
   # Upd .bashrc
   cp ~/.bashrc{,.bk}
@@ -93,17 +83,14 @@ install_vim_bash_custom() {
   curl -s https://gist.githubusercontent.com/spacegaucho/25c8b3f505484e16e1fc11d34075a297/raw > ~/.bash_aliases
   
   # Add tmux configs and cp neovim configs
+  set +x
 }
 
-main() {
-  if [[ $VIM_BASH_ONLY -eq 1 ]]; then
-    install_vim_bash_custom
-  else
-    install_workstuff ; install_vim_bash_custom
-  fi
-  if [[ $INSTALL_STARSHIP -eq 1 ]]; then
-    install_starship
-  fi
-}
-
-main
+if main &>> $COMMAND_OUT
+then 
+  echo "OK: setupp succesful"
+else
+  echo "ERR: Presse any key to see details"
+  less $COMMAND_OUT
+fi
+ 
