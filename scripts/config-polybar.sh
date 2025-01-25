7dafcbd91e6e5e18b6cdfc80aaca30fbf5bf26c1
spacegaucho <vodka3dg@gmail.com>
2025-01-25 02:29:16 -0300

    feat: added polybar installation and config

--- scripts/config-polybar.sh
+++ scripts/config-polybar.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-CONF_FILE="picom.conf"
-CONFS="./files/confs"
-DEST="${HOME}/.config"
+CONF_FILE="config.ini"
+CONFS="./files/confs/polybar"
+DEST="/etc/polybar"
 
 if [[ ! -d "${DEST}" ]]
 then
-  mkdir -p "${DEST}"
+  sudo mkdir -p "${DEST}"
 fi
 
 if [[ -L "${DEST}/${CONF_FILE}" ]]
 then
   echo "I: ${DEST}/${CONF_FILE} already exists and is link"
-  exit 0
 elif [[ -f "${DEST}/${CONF_FILE}" ]]
 then
   echo "I: ${DEST}/${CONF_FILE} already exists but is regular file, backing up"
   set -x
-  mv "${DEST}/${CONF_FILE}" "${DEST}/${CONF_FILE}${BACKUP_SFX}"
+  sudo mv "${DEST}/${CONF_FILE}" "${DEST}/${CONF_FILE}${BACKUP_SFX}"
   set +x
 else
-  ln -s "${DIR_GIT_REPO}/${CONFS}/${CONF_FILE}" "${DEST}/${CONF_FILE}"
+  sudo ln -s "${DIR_GIT_REPO}/${CONFS}/${CONF_FILE}" "${DEST}/${CONF_FILE}"
 fi
 
 
--- scripts/install-polybar.sh
+++ scripts/install-polybar.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo STUB
+
+REQ_DEB="build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libjsoncpp-dev"
+GIT_URL="https://github.com/polybar/polybar"
+GIT_TAG="3.7.2"
+APP_NAME="${GIT_URL##*/}"
+GIT_DIR="${HOME}/git"
+
+if [[ ! -d "${GIT_DIR}" ]]
+then
+  mkdir -p "${GIT_DIR}"
+fi
+
+git clone --single-branch --branch "${GIT_TAG}" "${GIT_URL}" "${GIT_DIR}/${APP_NAME}-${GIT_TAG}"
+
+sudo apt install -y ${REQ_DEB}
+
+pushd .
+
+cd "${GIT_DIR}/${APP_NAME}-${GIT_TAG}"
+
+./build.sh -f -A
+
+popd
+
+source ${BASE_NAME}/scripts/config-polybar.sh
