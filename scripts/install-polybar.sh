81df8b9de184fbcf3c4fe565a57fdf6ee4fcc6f7
spacegaucho <vodka3dg@gmail.com>
2025-02-16 23:54:57 -0300

    feat: polybar fixes

--- scripts/install-polybar.sh
+++ scripts/install-polybar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REQ_DEB="build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libjsoncpp-dev"
+REQ_DEB="build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libjsoncpp-dev libpulse-dev libiw-dev"
 GIT_URL="https://github.com/polybar/polybar"
 GIT_TAG="3.7.2"
 APP_NAME="${GIT_URL##*/}"
@@ -19,7 +19,7 @@ pushd .
 
 cd "${GIT_DIR}/${APP_NAME}-${GIT_TAG}"
 
-./build.sh -f -A
+./build.sh -f -p -A
 
 popd
 
