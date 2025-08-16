07c4028eb577a388e052ab2e058f4f87e492b4f8
spacegaucho <vodka3dg@gmail.com>
2025-08-15 22:34:29 -0300

    ft: fixed stuff

--- dotfilesync.sh
+++ dotfilesync.sh
@@ -5,13 +5,14 @@ export PATH="$HOME/desksetup:$HOME/desksetup/files/misc/:$PATH"
 
 CONFS="$HOME/git/desksetup/
 $HOME/.config/nvim/
+$HOME/.ssh/
 $HOME/.i3/
 $HOME/scripts/"
 
 for CONF_DIR in ${CONFS}
 do
   if [[ -d "${CONF_DIR}" ]]; then
-    msgOK "updating ${CONF_DIR}"
+    msgInfo "updating ${CONF_DIR}"
     git -C "${CONF_DIR}" pull
   else
     msgWarning "${CONF_DIR} does not exist in this host, skipping"
