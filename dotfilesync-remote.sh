b5cb9bd3195db433f8da77c25c33debed89a3b56
spacegaucho <vodka3dg@gmail.com>
2026-01-02 14:41:10 -0300

    M  dotfilesync-remote.sh

--- dotfilesync-remote.sh
+++ dotfilesync-remote.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -n "$(cat ./dotfiles-hosts)" | parallel -kd " " echo ==== Host: {} ==== \; ssh {} -- git -C desksetup pull \\\; TERM=xterm-256color ./desksetup/dotfilesync.sh \; echo
+echo -n "$(cat ./dotfiles-hosts)" | parallel -kd ' ' "echo ==== Host: {} ==== \; ssh {} -- git -C desksetup pull \\\; TERM=xterm-256color ~/desksetup/dotfilesync.sh \; echo"
