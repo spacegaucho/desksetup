fef65a40cecc353d6ccbd788dfa5b6479f989b50
spacegaucho <vodka3dg@gmail.com>
2026-01-25 16:41:33 -0300

    M  dotfilesync-remote.sh M  dotfilesync.sh

--- dotfilesync-remote.sh
+++ dotfilesync-remote.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -n "$(cat ./dotfiles-hosts)" | parallel -kd ' ' "echo ==== Host: {} ==== \; ssh {} -- git -C desksetup pull \\\; TERM=xterm-256color ~/desksetup/dotfilesync.sh \; echo"
+cat ./dotfiles-hosts | parallel -kd ' ' 'printf "\033[0;35mHost: %s\033[0m\n" {} ; ssh "{}" -- git -C desksetup pull \\\; TERM=xterm-256color ~/desksetup/dotfilesync.sh ; printf "\n"'
--- dotfilesync.sh
+++ dotfilesync.sh
@@ -13,8 +13,7 @@ for CONF_DIR in ${CONFS}
 do
   if [[ -d "${CONF_DIR}" ]]; then
     msgInfo "updating ${CONF_DIR}"
-    git -C "${CONF_DIR}" pull || git -C "${CONF_DIR}" pull --rebase || \
-    git -C "${CONF_DIR}" pull  --ff-only
+    git -C "${CONF_DIR}" pull
   else
     msgWarning "${CONF_DIR} does not exist in this host, skipping"
   fi
