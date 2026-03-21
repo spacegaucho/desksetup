6387acea46d771ededf8dd00b8adcbd50ad49238
spacegaucho <vodka3dg@gmail.com>
2026-03-20 23:21:26 -0300

    D  dotfilesync-remote.sh

--- dotfilesync-remote.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Directory of this script
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Read dotfiles-hosts file relative to script directory
-cat "$SCRIPT_DIR/dotfiles-hosts" | \
-  parallel -kd '\n' 'printf "\033[0;35mHost: %s\033[0m\n" {} ; ssh "{}" -- "git -C ~/desksetup pull ;git -C ~/scripts pull ; TERM=xterm-256color ~/scripts/sh/dotfilesync.sh 2>&1" ; printf "\n"'
-
